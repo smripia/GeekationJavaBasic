d9d083456279736ca39a06ee920669ba7ef684d2
Sumire Ohashi <s.ohashi.cingroup@gmail.com>
2025-03-08 22:44:15 +0900

    データ型の課題を実装

---
+++ src/basicSyntax/DateType.java
@@ -0,0 +1,16 @@
+package basicSyntax;
+
+public class DateType {
+
+	public static void main(String[] args) {
+		// TODO 自動生成されたメソッド・スタブ
+		System.out.println("こんにちは"+ "私は"+ "Geek君です。");
+		
+		System.out.println(10);
+		System.out.println("20" + "24");
+		System.out.println(3.14);
+		System.out.println(true);
+
+	}
+
+}
